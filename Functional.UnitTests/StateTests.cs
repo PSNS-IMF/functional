63f147dd83a65e2fd8394f6cc0a0554d361c5693
Jeff Purcell <japurcell@users.noreply.github.com>
2018-07-25 14:37:36 -0700

    Finished State

--- Functional.UnitTests/StateTests.cs
+++ Functional.UnitTests/StateTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using Psns.Common.Functional;
-using System;
+using System.Collections.Generic;
+using System.Linq;
 using static NUnit.StaticExpect.Expectations;
 using static Psns.Common.Functional.Prelude;
 
@@ -9,15 +10,45 @@ namespace SystemExtensions.UnitTests.Functional.StateUnitTests
     [TestFixture]
     public class StateTests
     {
-        public delegate BinaryState BinaryState();
+        public static State<T, IEnumerable<T>> Pop<T>() =>
+            source => (source.First(), source.Skip(1));
 
-        public static BinaryState On() =>
-            new BinaryState(Off);
+        public static State<T, IEnumerable<T>> Push<T>(T val) =>
+            source => (val, source.Append(val));
 
-        public static BinaryState Off() =>
-            new BinaryState(On);
+        [Test]
+        public void StateShouldBeUpdatable()
+        {
+            var query =
+                from v1 in Push(1)
+                from v2 in Push(3)
+                from v3 in Pop<int>()
+                from v4 in Push(2)
+                select v4;
 
-        public static State<object, BinaryState> Next() =>
-            ((object)null).State<object, BinaryState>(state => state ?? On());
+            var query2 =
+                from v1 in query
+                from v2 in Push(5)
+                select v2;
+
+            var result = query2(Empty<int>());
+
+            CollectionAssert.AreEqual(Cons(3, 2, 5), result.State);
+            Expect(result.Value, EqualTo(5));
+        }
+
+        [Test]
+        public void StateShouldBeChainable()
+        {
+            var initial =
+                State<int, IEnumerable<int>>()
+                    .Bind(val => state => (val + 1, state.Append(1)))
+                    .Bind(val => state => (val + 2, state.Append(2)));
+
+            var result = initial(Empty<int>());
+
+            CollectionAssert.AreEqual(Cons(1, 2), result.State);
+            Expect(result.Value, EqualTo(3));
+        }
     }
 }
--- Psns.Common.Functional/State.cs
+++ Psns.Common.Functional/State.cs
@@ -3,45 +3,70 @@ using static Psns.Common.Functional.Prelude;
 
 namespace Psns.Common.Functional
 {
-    public delegate Lazy<T, TState> State<T, TState>(TState state);
+    public delegate (TValue Value, TState State) State<TValue, TState>(TState state);
 
-    public static partial class StateModule
+    public static partial class StateExtensions
     {
+        public static State<TValue, TState> State<TValue, TState>(this TValue self) =>
+            state => (self, state);
+
+        public static State<TValue, TState> State<TValue, TState>(this TValue self, Func<TState, TState> newState) =>
+            oldState => (self, newState(oldState));
+
+        public static State<TValue, TState> Bind<TValue, TState>(
+            this State<TValue, TState> self,
+            Func<TValue, State<TValue, TState>> binder) =>
+                self.Bind<TValue, TValue, TState>(binder);
+
+        public static State<TResult, TState> Bind<TValue, TResult, TState>(
+            this State<TValue, TState> self,
+            Func<TValue, State<TResult, TState>> binder) => state => 
+                Map(self(state), res => binder(res.Value)(res.State));
+
         public static State<TResult, TState> SelectMany<TSource, TState, TSelector, TResult>(
-            this State<TSource, TState> source,
+            this State<TSource, TState> self,
             Func<TSource, State<TSelector, TState>> selector,
             Func<TSource, TSelector, TResult> resultSelector) => state =>
-                    Map(source(state), sourceRes =>
-                        Map(selector(sourceRes.Value)(sourceRes.Metadata), selectorRes =>
-                            new Lazy<TResult, TState>(() => 
-                                resultSelector(sourceRes.Value, selectorRes.Value), selectorRes.Metadata)));
+                Map(self(state), sourceRes =>
+                    Map(selector(sourceRes.Value)(sourceRes.State), selectorRes =>
+                        resultSelector(sourceRes.Value, selectorRes.Value)
+                            .State<TResult, TState>()(selectorRes.State)));
+
+        public static State<TResult, TState> Select<TSource, TResult, TState>(
+            this State<TSource, TState> self,
+            Func<TSource, TResult> map) =>
+                self.SelectMany(
+                    value => map(value).State<TResult, TState>(),
+                    (_, selectVal) => selectVal);
     }
 
     public static partial class Prelude
     {
-        public static State<T, TState> State<T, TState>(this T value) => state =>
-            new Lazy<T, TState>(() => value, state);
+        public static State<TValue, TState> State<TValue, TState>() =>
+            state => (default(TValue), state);
 
-        public static State<UnitValue, TState> UnitState<TState>() =>
+        public static State<TValue, TState> State<TValue, TState>(TValue val) =>
+            state => (val, state);
+
+        public static State<TValue, TState> State<TValue, TState>(Func<TState, (TValue, TState)> func) =>
+            state => func(state);
+
+        public static State<UnitValue, TState> State<TState>(Action action) => state =>
+        {
+            action();
+            return (Unit, state);
+        };
+
+        public static State<UnitValue, TState> State<TState>() =>
             Unit.State<UnitValue, TState>();
 
-        public static State<TResult, TState> Select<TSource, TResult, TState>(
-            this State<TSource, TState> source,
-            Func<TSource, TResult> selector) =>
-                source.SelectMany(
-                    value => selector(value).State<TResult, TState>(), 
-                    (_, selectVal) => selectVal);
+        public static State<TState, TState> GetState<TState>() => state =>
+           (state, state);
 
-        public static TSource Value<TSource, TState>(this State<TSource, TState> source, TState state) =>
-            source(state).Value;
+        public static State<TState, TState> SetState<TState>(TState newState) => oldState =>
+            (oldState, newState);
 
-        public static TState State<T, TState>(this State<T, TState> source, TState state) =>
-            source(state).Metadata;
-
-        public static State<TState, TState> Get<TState>() => state =>
-           new Lazy<TState, TState>(() => state, state);
-
-        public static State<TState, TState> Set<TState>(Func<TState, TState> newState) => oldState =>
-            new Lazy<TState, TState>(() => oldState, newState(oldState));
+        public static State<UnitValue, TState> SetState<TState>(Func<TState, TState> newState) => oldState =>
+            (Unit, newState(oldState));
     }
 }
